b200dbaddf08ec874ee9dbea2ed2a0dd10be18d3
Joohwan Lee <aperire0402@gmail.com>
2022-07-08 23:52:04 +0900

    Add Token request

--- backend.py
+++ backend.py
@@ -18,5 +18,14 @@ def request_blocktree():
     return json_dump
 
 
+@app.route("/tokens", methods=["GET"])
+def request_token():
+    args = request.args
+    account = args.get("account", type=str)
+    data_set = BlockTree().get_tokens(account)
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+
 if __name__ == "__main__":
     app.run()
